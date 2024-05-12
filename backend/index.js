5551dd173f5dded78b848aa724a9d72f0810105b
Jonathan Deng <jonathandengq34@gmail.com>
2024-05-12 02:04:30 -0700

    updated register and forgot password endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,13 @@
 
 import constants from './constants.js'
 import express from 'express'
-
+import fs from 'fs'
 //const express = require('express')
+//const fs = require('fs');
 console.log(constants)
 const app = express()
+let users = JSON.parse(fs.readFileSync('./data/users.json'));
+let emails = JSON.parse(fs.readFileSync('./data/emailconfirmation.json'))
 
 app.use(express.json())
 
@@ -28,6 +31,49 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    console.log(req.body);
-    res.send('Received user, pass, and email');
+   // console.log(req.body);
+    const newId = users[users.length-1].id+1;
+    const newUser = Object.assign({id: newId}, req.body);
+    users.push(newUser);
+    fs.writeFile('./data/users.json', JSON.stringify(users), (err) => {
+        res.status(201).json({
+            status: "success"
+        })
+    });
+});
+
+
+app.get('/forgotpassword', (req, res) => {
+    res.json()
+});
+app.post('/forgotpassword', (req, res) => {
+     const newId = emails[emails.length-1].id+1;
+     const newEmail = Object.assign({id: newId}, req.body);
+     emails.push(newEmail);
+     fs.writeFile('./data/emailconfirmation.json', JSON.stringify(emails), (err) => {
+         res.status(201).json({
+             status: "success"
+         })
+     });
+ });
+
+ app.get('/getuser/:id', (req, res) => {
+    const id = req.params.id;
+    // const user = {
+    //     id: 1,
+    //     username: "blah balh",
+    // }
+    const user = users[0];
+    res.send({
+        user: user,
+    });
+});
+
+app.get('/getbook', (req, res) => {
+    res.json()
+});
+
+
+app.get('/getbookexchanges', (req, res) => {
+    res.json()
 });
