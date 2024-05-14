f03159ceae571e98ba5b28e98124508ab531845e
sohanagarkar <sohanagarkar@gmail.com>
2024-05-14 16:07:26 -0700

    Finished GET Book by ID and endpoint and built rough draft of bbook upload endpoint (no images as of now)

--- backend/controllers/book_controller.js
+++ backend/controllers/book_controller.js
@@ -10,20 +10,45 @@ This controller manages all endpoints that have to do with the retrieval
 and posting of book information.
 */
 
+/**
+ * Takes in the Object ID of a book and returns a JSON object of that book.
+ * Replaces the bookOwner field in the database document from the owner's
+ * Object ID to an object with the owner's Object ID and username.
+ */
 router.get('/get/:id', (req, res) => {
     Book.findOne({
         _id: req.params.id
-    }).then((book) => {
+    }).populate('bookOwner', '_id username').then((book) => {
         if(!book) {
             res.status(404);
             res.send("Book Not Found!");
         }
-        //TODO: Aggregate User Data into the Book document being sent
         res.send(book);
     }).catch((e) => {
         res.status(500);
         //TODO: Remove this. We don't want to send internal errors to the client
         res.send(e);
+    });
+});
+
+/**
+ * Take in the JSON for a new book and upload it under the user who posted it
+ * Since JWT isn't implemented yet, I'm hardcoding the owner right now.
+ * 
+ * Expected Body:
+ * Title, Author, Genre
+ */
+//TODO: Replace the Hardcoded User with the User Encoded in JWT Token
+router.post('/upload', (req, res) => {
+    let newBook = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        isBookOutForExchange: false,
+        bookOwner: new mongoose.Types.ObjectId('6643d77345389a92052ed220')
+    });
+    newBook.save().then(doc => {
+        res.send("Upload Successful");
     });
 });
 
