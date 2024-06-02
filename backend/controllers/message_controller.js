cb879955f581eabc24882ce6ba526ce3262240f8
Danniell Xu <danniell@Danniells-MacBook-Air.local>
2024-06-02 04:17:38 -0700

    Update message post controller to include more error handling, added get controller for messages. -Note: The error handling was copied from the book controller, but crashed the server once it came across an error. I fixed this by making the function async, but it is best to update all functions with error handling to be async since the current method of error handling crashes the server.

--- backend/controllers/message_controller.js
+++ backend/controllers/message_controller.js
@@ -6,8 +6,66 @@ import Message from '../db_models/message_model.js'
 
 const router = express.Router();
 
-router.post('/send/:id', validateJWT(), (req, res) => {
+router.get('/:id', validateJWT(), async (req, res) => {
     try {
+            let sendUser = await User.findById(req.userId);
+            if (!sendUser) {
+                res.status(404);
+                res.json({
+                    "reason": "Sender Doesn't Exist"
+                });
+                return;
+            }
+            let recvUser = await User.findById(req.params.id);
+            if (!recvUser) {
+                res.status(404);
+                res.json({
+                    "reason": "Recipient Doesn't Exist"
+                });
+                return;
+            }
+            const messageHistory = await Message.find({
+                $and: [
+                    { senderID: new mongoose.Types.ObjectId(req.userId) },
+                    { recipientID: new mongoose.Types.ObjectId(req.params.id) }
+                ]
+            });
+            res.status(200);
+            res.json(messageHistory);
+            return;
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Internal Server Error");      
+    }
+    return;
+});
+
+router.post('/send/:id', validateJWT(), async (req, res) => {
+    try {
+            if (req.params.id == req.userId)
+            {
+                res.status(400);
+                res.json({
+                    "reason": "Message With Self"
+                });
+                return;
+            }
+            let sendUser = await User.findById(req.userId);
+            if (!sendUser) {
+                res.status(404);
+                res.json({
+                    "reason": "Sender Doesn't Exist"
+                });
+                return;
+            }
+            let recvUser = await User.findById(req.params.id);
+            if (!recvUser) {
+                res.status(404);
+                res.json({
+                    "reason": "Recipient Doesn't Exist"
+                });
+                return;
+            }
             let newMessage = new Message({
                 senderID: req.userId,
                 recipientID: req.params.id,
@@ -20,6 +78,7 @@ router.post('/send/:id', validateJWT(), (req, res) => {
         console.log(e);
         res.status(500).send("Internal Server Error");      
     }
+    return;
 });
 
 
